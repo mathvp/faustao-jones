879c929e85284e64665ff3ee36360f144e8daf72
Matheus Vieira <matheusvieiradepaula@gmail.com>
2020-08-13 00:51:03 -0300

    Add game main mechanic

--- js/screens/gameScreen.js
+++ js/screens/gameScreen.js
@@ -4,11 +4,19 @@ import words from '../../data/words.js';
 export class GameScreen extends BaseScreen {
   constructor(options) {
     super(options);
-    const wordObj = {}
+    this.wordObj = {}
+    this.guessWord = [];
+    this.currentErrors = 0;
+    this.maxErrors = null;
   }
 
   setupGame() {
     this.wordObj = this.getRandomWordObj();
+    this.maxErrors = Math.ceil(this.wordObj.length / 2);
+
+    for(let i = 0; i < this.wordObj.length; i++) {
+      this.guessWord.push('_ ');
+    }
   }
 
   getRandomWordObj() {
@@ -21,5 +29,44 @@ export class GameScreen extends BaseScreen {
   startGame() {
     this.setupGame();
     this.show(1000);
+
+    this.printGuessWord();
+
+    document.addEventListener('letterClick', (event) => {
+      if (!this.lose()) {
+        this.compareLetterWord(event.detail, 0, 0);
+      }
+    });
   }
+
+  printGuessWord() {
+    const container = document.getElementById('guess-container');
+    container.innerHTML = this.guessWord.join('');
+  }
+
+  compareLetterWord(letter, start, hits) {
+    const lowerCaseWord = this.wordObj.word.toLowerCase();
+    const letterIndex = lowerCaseWord.indexOf(letter.toLowerCase(), start);
+
+    if (letterIndex >= 0) {
+      this.guessWord[letterIndex] = letter;
+      this.printGuessWord();
+      this.compareLetterWord(letter, letterIndex + 1, hits + 1);
+    } else if (hits == 0){
+      this.currentErrors += 1;
+      if (this.lose()) {
+        this.gameOver();
+      }
+
+    }
+  }
+
+  lose() {
+    return (this.maxErrors - this.currentErrors) == 0;
+  }
+
+  gameOver() {
+    alert('Perdeu');
+  }
+
 }
--- js/screens/letterBoard.js
+++ js/screens/letterBoard.js
@@ -10,10 +10,27 @@ export class LetterBoard {
     letters.forEach((letter) => {
       const letterElement = document.createElement('div');
       letterElement.setAttribute('class', `letter letter-${letter}`);
+      letterElement.setAttribute('data-value', `${letter}`);
       letterElement.append(`${letter}`);
+
+      letterElement.addEventListener('click', (event) => {
+        this.selectLetter(event.target);
+      });
       this.element.appendChild(letterElement);
     });
   }
 
+  selectLetter(letterElement) {
+    if (letterElement.dataset.disabled) {
+      return;
+    }
 
+    const clickedLetter = letterElement.dataset.value;
+
+    letterElement.classList.add('clicked')
+    letterElement.setAttribute('data-disabled', 'true');
+
+    const event = new CustomEvent('letterClick', { detail: clickedLetter });
+    document.dispatchEvent(event);
+  }
 }
